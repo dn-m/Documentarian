f37fdb8c7d33cff98c478c1f9a74b85310912392
James <jbean46@gmail.com>
2018-08-05 12:22:29 -0700

    Use straight bash script instead of program + args

--- Sources/Documentarian/main.swift
+++ Sources/Documentarian/main.swift
@@ -14,7 +14,7 @@ struct Product: Decodable {
 
 /// - Returns: A `Package` for the given Swift Package repository.
 func decodePackage() throws -> Package {
-    let data = run("swift", "package", "dump-package").stdout.data(using: .utf8)!
+    let data = run(bash: "swift package dump-package").stdout.data(using: .utf8)!
     let decoder = JSONDecoder()
     return try decoder.decode(Package.self, from: data)
 }
