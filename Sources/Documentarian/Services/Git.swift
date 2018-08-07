66a3eb606488bb193205aaf6de2a380564a3e4aa
James <jbean46@gmail.com>
2018-08-06 19:39:24 -0700

    You gotta add and config

--- Sources/Documentarian/Services/Git.swift
+++ Sources/Documentarian/Services/Git.swift
@@ -8,7 +8,6 @@
 import SwiftShell
 
 func commitUpdates(for package: Package, with token: String) throws {
-    try runAndPrint(bash: "git -c user.name='jsbean' -c user.email='\(token)'")
-    try runAndPrint(bash: "add -A")
+    try runAndPrint(bash: "git -c user.name='jsbean' -c user.email='\(token)' add -A")
     try runAndPrint(bash: "commit -m 'Update documentation for the \(package.name) package'")
 }
